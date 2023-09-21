8e2da9f64c6e8d318d7875b1f9677a4625a9b178
Madari Zoltán <zoltan.madari@zzsys.hu>
2023-09-21 12:15:53 +0200

    feat: base

---
+++ resources/views/calendar.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pepita Calendar</title>
+    <link rel="stylesheet" href="{{ Vite::asset('resources/css/app.css') }}">
+</head>
+<body>
+    <div id="box"></div>
+    <div id='calendar'></div>
+    <script type="text/javascript" src="{{ Vite::asset('resources/js/app.js') }}"></script>
+</body>
+</html>
